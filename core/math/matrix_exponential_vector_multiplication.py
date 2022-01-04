8c54a3150736d427c5eec78d0a9a6bbd03390dd9
yunweilu <yunweilu2020@u.northwestern.edu>
2022-01-03 19:33:30 -0600

    Update of exponential matrix for jax

--- core/math/matrix_exponential_vector_multiplication.py
+++ core/math/matrix_exponential_vector_multiplication.py
@@ -5,7 +5,7 @@ Action of matrix exponential and action of propagator derivative on a vector or 
 import numpy as np
 import scipy.sparse.linalg
 from scipy.sparse import bmat, isspmatrix
-
+import jax.numpy as jnp
 
 def expmat_der_vec_mul(A, E, tol, state):
     """
@@ -51,6 +51,16 @@ def one_norm(A):
     else:
         return np.linalg.norm(A, 1)
 
+def one_norm_ad(A):
+    """
+    Used for jax.
+    Args:
+    A :: numpy.ndarray - matrix or vector
+    Returns:
+    one norm of A
+    """
+
+    return jnp.linalg.norm(A, 1)
 
 def trace(A):
     """
@@ -63,6 +73,16 @@ def trace(A):
         return A.diagonal().sum()
     else:
         return np.trace(A)
+
+def trace_ad(A):
+    """
+    Used for jax.
+    Args:
+    A :: numpy.ndarray - matrix
+    Returns:
+    trace of A
+    """
+    return jnp.trace(A)
 
 
 def ident_like(A):
@@ -78,6 +98,15 @@ def ident_like(A):
     else:
         return np.eye(A.shape[0], A.shape[1], dtype=A.dtype)
 
+def ident_like_ad(A):
+    """
+    Used for jax.
+    Args:
+    A :: numpy.ndarray - matrix
+    Returns:
+    Identity matrix which has same dimension as A
+    """
+    return jnp.eye(A.shape[0], A.shape[1], dtype=A.dtype)
 
 def get_s(A,tol):
     """
@@ -100,6 +129,32 @@ def get_s(A,tol):
             if max_power > 30:
                 break
         max_power = np.ceil(np.log10(max_term))
+        if max_power - 16 <= tol_power:
+            break
+        s = s + 1
+    return s
+
+def get_s_ad(A,tol):
+    """
+    Determine s in scaling and squaring. Used for jax.
+    Args:
+    A :: numpy.ndarray - matrix
+    tol :: numpy.float64 - expected error
+    Returns:
+    s :: int
+    """
+    s = 1
+    while (1):
+        tol_power = jnp.ceil(jnp.log10(tol))
+        norm_A = one_norm(A) / s
+        max_term_notation = jnp.floor(norm_A)
+        max_term = 1
+        for i in range(1, jnp.int64(max_term_notation)):
+            max_term = max_term * norm_A / i
+            max_power = jnp.ceil(jnp.log10(max_term))
+            if max_power > 30:
+                break
+        max_power = jnp.ceil(jnp.log10(max_term))
         if max_power - 16 <= tol_power:
             break
         s = s + 1
@@ -155,6 +210,52 @@ def expmat_vec_mul(A, b, tol=None):
         b = f
     return f
 
-a=np.ones((2,2))
-b=np.ones((2,1))
-expmat_der_vec_mul(a,a,2**-53,b)
+
+def expmat_vec_mul_ad(A, b, tol=None):
+    """
+    Compute the exponential matrix and vector multiplication e^(A) B. Used for jax.
+    Args:
+    A :: numpy.ndarray - matrix
+    b :: numpy.ndarray - vector or a set of basis vectors
+    tol :: numpy.float64 - expected error
+    Returns:
+    f :: numpy.ndarray - Approximation of e^(A) b
+    """
+    ident = ident_like_ad(A)
+    n = A.shape[0]
+    mu = trace_ad(A) / float(n)
+    # Why mu? http://eprints.ma.man.ac.uk/1591/, section 3.1
+    A = A - mu * ident
+    if tol is None:
+        tol = 1e-16
+    s = get_s_ad(A,tol)
+    f = b
+    j = 0
+    while (1):
+        eta = jnp.exp(mu / float(s))
+        coeff = s * (j + 1)
+        b = jnp.matmul(A,b) / coeff
+        c2 = one_norm_ad(b)
+        f = f + b
+        total_norm = one_norm_ad(f)
+        if c2 / total_norm < tol:
+            m = j + 1
+            break
+        j = j + 1
+    f = eta * f
+    b = f
+    for i in range(1, int(s)):
+        eta = jnp.exp(mu / float(s))
+        for j in range(m):
+            coeff = s * (j + 1)
+            b = jnp.matmul(A,b)  / coeff
+            c2 = one_norm_ad(b)
+            f = f+ b
+            total_norm = one_norm_ad(f)
+            if c2 / total_norm < tol:
+                m = j + 1
+                break
+        f = eta * f
+        b = f
+    return f
+
